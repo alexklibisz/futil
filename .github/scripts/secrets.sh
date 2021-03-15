ae56a7976b02915e6a31917e9735f2131159e64c
Alex Klibisz <aklibisz@gmail.com>
2021-03-14 21:49:45 -0400

    Cross publish and set gpg permissions

--- .github/scripts/secrets.sh
+++ .github/scripts/secrets.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 mkdir -p $HOME/.gnupg $HOME/.sbt/1.0
-chown -R $(whoami) ~/.gnupg/
 echo $GPG_SECRET_B64 | base64 --decode > $HOME/.gnupg/secring.gpg
 echo $SBT_SONATYPE_B64 | base64 --decode > $HOME/.sbt/1.0/sonatype.sbt
+chown -R $(whoami) ~/.gnupg/
+find ~/.gnupg -type f -exec chmod 600 {} \;
+find ~/.gnupg -type d -exec chmod 700 {} \;

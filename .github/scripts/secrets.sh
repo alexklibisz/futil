a33fc5db6670902d9208f490eecda27e015c7b75
Alex Klibisz <aklibisz@gmail.com>
2021-03-14 21:46:09 -0400

    Chown gnupg dir.

--- .github/scripts/secrets.sh
+++ .github/scripts/secrets.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 mkdir -p $HOME/.gnupg $HOME/.sbt/1.0
+chown -R $(whoami) ~/.gnupg/
 echo $GPG_SECRET_B64 | base64 --decode > $HOME/.gnupg/secring.gpg
 echo $SBT_SONATYPE_B64 | base64 --decode > $HOME/.sbt/1.0/sonatype.sbt

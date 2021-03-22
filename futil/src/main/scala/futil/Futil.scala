7922c182a0f0fe6e866021e525975789f6201e5c
Alex Klibisz <aklibisz@gmail.com>
2021-03-21 22:12:07 -0400

    Rewrote traverse methods to have type signature more like Future.traverse

--- futil/src/main/scala/futil/Futil.scala
+++ futil/src/main/scala/futil/Futil.scala
@@ -1,10 +1,11 @@
 package futil
 
 import java.util.concurrent.{Callable, Executors, ScheduledExecutorService, ThreadFactory}
-import scala.concurrent.Future.fromTry
+import scala.collection.compat._
+import scala.concurrent.Future.{fromTry, successful}
 import scala.concurrent._
 import scala.concurrent.duration._
-import scala.util.Try
+import scala.util._
 
 /** Utilities to get more from Scala Futures. */
 object Futil {
@@ -71,22 +72,29 @@ object Futil {
     delay(duration)(Future.successful(()))
 
   /**
-    * Use function f to map each element in as to a Future[B], running at most n Futures at a time.
+    * Use function f to map each element from `in` to a Future[B], running at most n Futures at a time.
     * Lifts the result of the Future[B] into a Future[Try[B]\] to prevent failing the entire Seq.
     * Results are returned in the original order.
     */
-  final def traverseParN[A, B](
+  final def traverseParN[A, B, M[X] <: IterableOnce[X]](
       n: Int
-  )(as: Seq[A])(f: A => Future[B])(implicit ec: ExecutionContext): Future[Seq[Try[B]]] = {
+  )(in: M[A])(fn: A => Future[B])(implicit bf: BuildFrom[M[A], Try[B], M[Try[B]]],
+                                  ec: ExecutionContext): Future[M[Try[B]]] = {
     val sem = semaphore(n)
-    Future.sequence(as.map(a => sem.withPermit(thunk(f(a).transformWith(Future.successful)))))
+    in.iterator
+      .foldLeft(successful(bf.newBuilder(in))) { (f1, a: A) =>
+        val f2 = sem.withPermit(thunk(fn(a).transformWith(Future.successful)))
+        f1.zipWith(f2)(_ += _)
+      }
+      .map(_.result())(ec)
   }
 
   /**
     * Alias for [[traverseParN]] with n = 1.
     */
-  final def traverseSerial[A, B](as: Seq[A])(f: A => Future[B])(implicit ec: ExecutionContext): Future[Seq[Try[B]]] =
-    traverseParN(1)(as)(f)
+  final def traverseSerial[A, B, M[X] <: IterableOnce[X]](in: M[A])(
+      fn: A => Future[B])(implicit bf: BuildFrom[M[A], Try[B], M[Try[B]]], ec: ExecutionContext): Future[M[Try[B]]] =
+    traverseParN(1)(in)(fn)
 
   /**
     * Retry the given Future according to the given [[RetryPolicy]].
